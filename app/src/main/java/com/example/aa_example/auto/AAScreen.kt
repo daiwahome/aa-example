4728bd8c0097c8d3f4c6158048587cf6be344749
daiwahome <daiwahome0@gmail.com>
2025-07-30 11:39:36 +0900

    Get current location

--- app/src/main/java/com/example/aa_example/auto/AAScreen.kt
+++ app/src/main/java/com/example/aa_example/auto/AAScreen.kt
@@ -1,8 +1,11 @@
 package com.example.aa_example.auto
 
+import android.content.Context
+import android.location.LocationManager
 import android.text.SpannableString
 import android.text.Spanned
 import androidx.car.app.CarContext
+import androidx.car.app.CarToast
 import androidx.car.app.Screen
 import androidx.car.app.model.Action
 import androidx.car.app.model.CarLocation
@@ -19,11 +22,27 @@ import com.example.aa_example.db.places
 
 class AAScreen(carContext: CarContext) : Screen(carContext) {
 
+    private val locationManager: LocationManager = carContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
     // フォールバック用の位置情報
     private val fallbackLocation = 35.6762 to 139.6503
 
     private fun getCurrentLocation(): Pair<Double, Double> {
-        return fallbackLocation
+        return try {
+            val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if (location == null) {
+                CarToast.makeText(carContext, "位置情報の取得に失敗しました。", CarToast.LENGTH_SHORT).show()
+                return fallbackLocation
+            }
+
+            location.latitude to location.longitude
+        } catch (e: SecurityException) {
+            CarToast.makeText(carContext, "位置情報の権限がありません。", CarToast.LENGTH_SHORT).show()
+            fallbackLocation
+        } catch (e: Exception) {
+            CarToast.makeText(carContext, "位置情報の取得に失敗しました。", CarToast.LENGTH_SHORT).show()
+            fallbackLocation
+        }
     }
 
     override fun onGetTemplate(): Template {
